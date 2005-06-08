2300da0960090374c7becc15603b105642d0a7e7
thomas <thomasokken@gmail.com>
2005-06-08 20:40:13 +0000

    *** empty log message ***

--- src/jdbcnav/javascript/JavaScriptPreparedStatement.java
+++ src/jdbcnav/javascript/JavaScriptPreparedStatement.java
@@ -65,7 +65,12 @@ public class JavaScriptPreparedStatement implements Scriptable {
 			if (args[2] instanceof Number)
 			    type = ((Number) args[2]).intValue();
 			else
-			    type = typeStringToInt((String) args[2]);
+			    try {
+				type = MiscUtils.sqlTypeStringToInt(
+							(String) args[2]);
+			    } catch (IllegalArgumentException e) {
+				throw new EvaluatorException(e.getMessage());
+			    }
 			if (args.length == 3)
 			    pstmt.setObject(((Number) args[0]).intValue(),
 					    args[1],
@@ -87,79 +92,6 @@ public class JavaScriptPreparedStatement implements Scriptable {
 					   + "optional third parameter.");
 	    return Context.getUndefinedValue();
 	}
-    }
-
-    private static int typeStringToInt(String type) {
-	if (type.equalsIgnoreCase("ARRAY"))
-	    return Types.ARRAY;
-	else if (type.equalsIgnoreCase("BIGINT"))
-	    return Types.BIGINT;
-	else if (type.equalsIgnoreCase("BINARY"))
-	    return Types.BINARY;
-	else if (type.equalsIgnoreCase("BIT"))
-	    return Types.BIT;
-	else if (type.equalsIgnoreCase("BLOB"))
-	    return Types.BLOB;
-	else if (type.equalsIgnoreCase("BOOLEAN"))
-	    return Types.BOOLEAN;
-	else if (type.equalsIgnoreCase("CHAR"))
-	    return Types.CHAR;
-	else if (type.equalsIgnoreCase("CLOB"))
-	    return Types.CLOB;
-	else if (type.equalsIgnoreCase("DATALINK"))
-	    return Types.DATALINK;
-	else if (type.equalsIgnoreCase("DATE"))
-	    return Types.DATE;
-	else if (type.equalsIgnoreCase("DECIMAL"))
-	    return Types.DECIMAL;
-	else if (type.equalsIgnoreCase("DISTINCT"))
-	    return Types.DISTINCT;
-	else if (type.equalsIgnoreCase("DOUBLE"))
-	    return Types.DOUBLE;
-	else if (type.equalsIgnoreCase("FLOAT"))
-	    return Types.FLOAT;
-	else if (type.equalsIgnoreCase("INTEGER"))
-	    return Types.INTEGER;
-	else if (type.equalsIgnoreCase("JAVA_OBJECT"))
-	    return Types.JAVA_OBJECT;
-	else if (type.equalsIgnoreCase("LONGVARBINARY"))
-	    return Types.LONGVARBINARY;
-	else if (type.equalsIgnoreCase("LONGVARCHAR"))
-	    return Types.LONGVARCHAR;
-	else if (type.equalsIgnoreCase("NULL"))
-	    return Types.NULL;
-	else if (type.equalsIgnoreCase("NUMERIC"))
-	    return Types.NUMERIC;
-	else if (type.equalsIgnoreCase("OTHER"))
-	    return Types.OTHER;
-	else if (type.equalsIgnoreCase("REAL"))
-	    return Types.REAL;
-	else if (type.equalsIgnoreCase("REF"))
-	    return Types.REF;
-	else if (type.equalsIgnoreCase("SMALLINT"))
-	    return Types.SMALLINT;
-	else if (type.equalsIgnoreCase("STRUCT"))
-	    return Types.STRUCT;
-	else if (type.equalsIgnoreCase("TIME"))
-	    return Types.TIME;
-	else if (type.equalsIgnoreCase("TIMESTAMP"))
-	    return Types.TIMESTAMP;
-	else if (type.equalsIgnoreCase("TINYINT"))
-	    return Types.TINYINT;
-	else if (type.equalsIgnoreCase("VARBINARY"))
-	    return Types.VARBINARY;
-	else if (type.equalsIgnoreCase("VARCHAR"))
-	    return Types.VARCHAR;
-	else
-	    throw new EvaluatorException(
-		      "Type must be one of \"ARRAY\", \"BIGINT\", \"BINARY\", "
-		    + "\"BIT\", \"BLOB\", \"BOOLEAN\", \"CHAR\", \"CLOB\", "
-		    + "\"DATALINK\", \"DATE\", \"DECIMAL\", \"DISTINCT\", "
-		    + "\"DOUBLE\", \"FLOAT\", \"INTEGER\", \"JAVA_OBJECT\", "
-		    + "\"LONGVARBINARY\", \"LONGVARCHAR\", \"NULL\", "
-		    + "\"NUMERIC\", \"OTHER\", \"REAL\", \"REF\", "
-		    + "\"SMALLINT\", \"STRUCT\", \"TIME\", \"TIMESTAMP\", "
-		    + "\"TINYINT\", \"VARBINARY\", or \"VARCHAR\".");
     }
 
     public void delete(int index) {
