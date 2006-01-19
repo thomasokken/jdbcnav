71d70242a1b33bb34397c56a539c43501cc7fbde
thomas <thomasokken@gmail.com>
2006-01-19 15:37:54 +0000

    *** empty log message ***

---
+++ src/jdbcnav/schemaedit/SchemaEditor.java
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////
+// JDBC Navigator - A Free Database Browser and Editor
+// Copyright (C) 2001-2006  Thomas Okken
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License, version 2,
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////
+
+package jdbcnav.schemaedit;
+
+import java.util.*;
+import jdbcnav.BrowserFrame;
+import jdbcnav.MyFrame;
+import jdbcnav.model.*;
+
+
+public class SchemaEditor extends MyFrame {
+    public SchemaEditor(BrowserFrame browser, Collection initialNodes) {
+	super(browser.getTitle() + " Schema Editor", true, true, true, true);
+    }
+}
